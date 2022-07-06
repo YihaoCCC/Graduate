740a15d908545cea7ab7937ea00df5fcf5ed992f
YihaoCCC <yihao168830@163.com>
2022-07-06 10:35:48 +0800

    ✨ feat:
    git-commit-plugin的基本使用

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 	<div class="welcome"  id="indexframe" ref="showModel" :class="show ? 'animate__animated animate__fadeOut' : ''">
         <!-- <div class="animate__animated animate__backInDown welcomeText animate__slow">
             <h1>
-                欢迎来到小奕在线办公服务系统
+                欢迎来到小奕在线办公服务系统 (mater内容更改、git-commit-plugin插件的使用)
             </h1>
         </div> -->
 		<yh-welcome welcomeText='欢迎来到小奕在线办公服务系统'></yh-welcome>
